846d07a0a13a9562eadadcd791df4647611eec39
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-02-03 16:37:36 -0200

    Changing folder name

--- back-end/slim/src/middleware.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Application middleware
-
-// e.g: $app->add(new \Slim\Csrf\Guard);
-$app->add(new Tuupola\Middleware\CorsMiddleware);
--- front-end/js/app/constant/index.constants.js
+++ front-end/js/app/constant/index.constants.js
@@ -1,5 +1,5 @@
 angular.module('lifeboat')
 .constant('API', {
-    // url: 'http://localhost:8080/'
-    url: 'https://verbonoslares.000webhostapp.com/slim/public/'
+    url: 'http://localhost:8080/'
+    // url: 'https://verbonoslares.000webhostapp.com/slim/public/'
 });
