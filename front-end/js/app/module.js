4b1961fc10f1a43dd49f7088980533f21152d7be
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-02-03 22:20:42 -0200

    Better CDN

--- front-end/js/app/module.js
+++ front-end/js/app/module.js
@@ -4,4 +4,14 @@ angular.module('lifeboat',[
     'ngMaterial', 
     'ngMessages',
     'ngAnimate'
-]);
+])
+// Date configuration format (DD-MM-YYYY): https://stackoverflow.com/questions/33475874/md-datepicker-input-format?rq=1
+.config(function($mdDateLocaleProvider) {
+    $mdDateLocaleProvider.formatDate = function(date) {
+        return date ? moment(date).format('DD-MM-YYYY') : '';
+    };
+    $mdDateLocaleProvider.parseDate = function(dateString) {
+        const m = moment(dateString, 'DD-MM-YYYY', true);
+        return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+});
