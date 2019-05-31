29d272d31c8689631f23b0f39a6ff7d2c84e69e9
Marcelo Rodrigues <marcelodive@hotmail.com>
2019-05-30 22:04:56 -0300

    Changing file places

--- front-end/js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    console.log("Hosted by 000webhost");
-    const divs = document.getElementsByTagName('div');
-    for (const div of divs) { 
-        if (div.innerHTML.includes('000webhost')) div.remove(); 
-    }
-};
